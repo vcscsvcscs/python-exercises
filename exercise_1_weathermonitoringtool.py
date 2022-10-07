3f906337ba0e9ba243b2e8f044cc3b1ded250726
Vargha Csongor <Culisa1023@gmail.com>
2022-10-07 14:04:30 +0200

    Time until the next sunrise or sun set + test

--- exercise_1_weathermonitoringtool.py
+++ exercise_1_weathermonitoringtool.py
@@ -1,4 +1,5 @@
-from math import floor
+import datetime
+import time
 import requests
 #Queries the current weather info from external service (https://openweathermap.org/current), returns dictioanry with weather info about the choosen city
 def query_weather_data(city,api_key):
@@ -14,3 +15,14 @@ def query_weather_data(city,api_key):
 #takes a temperature in kelvin and returns it in celsius,fahrenheit tuple
 def temperature_to_celsius_and_fahrenheit(temp):
     return (round(temp-273.15,2),round((temp - 273.15)* (9/5) + 32,2))
+
+def time_until_sunrise_or_sundown(sunrise,sunset):
+    untilSunrise = sunrise - datetime.datetime.now().timestamp()
+    untilSunset = sunset - datetime.datetime.now().timestamp()
+    if(0 < untilSunset and untilSunrise < 0):
+        return ("until_sunset",time.strftime("%H:%M:%S", time.gmtime(untilSunset)))
+    else:
+        if(0 < untilSunrise):
+            return ("until_sunrise",time.strftime("%H:%M:%S", time.gmtime(untilSunrise)))
+        else:
+            return ("until_sunrise",time.strftime("%H:%M:%S", time.gmtime(24 *datetime.time.hour+untilSunrise)))
