2f05c7acae160cb2a02365d49be70771d5a0b71d
Vargha Csongor <Culisa1023@gmail.com>
2022-10-08 20:43:48 +0200

    weathertool apis and tests

--- exercise_1_test_wmt_server.py
+++ exercise_1_test_wmt_server.py
@@ -1,20 +1,63 @@
+import datetime
 import unittest
+from urllib import response
 from exercise_1_wmt_server import wmt_server
+from exercise_1_weathermonitoringtool import query_weather_data
 from webtest import TestApp
 
+
 class TestWMTServer(unittest.TestCase):
+    city = "Budapest"
+    # this api key is under the acount of the author of this snippet and is solely for testing purposes,
+    # it blocks after 60 requests per minute and 1000 requests per day
+    api_key = "76717af25a09a9c55ca00b44f771acc0"
+    test_app = TestApp(wmt_server)
+
     def test_query_weather_data(self):
-        city = "Budapest"
-        #this api key is under the acount of the author of this snippet and is solely for testing purposes, it blocks after 60 requests per minute and 1000 requests per day
-        api_key = "76717af25a09a9c55ca00b44f771acc0"
-        test_app = TestApp(wmt_server)
-        response = test_app.get('/weatherdata?city=%s&api_key=%s' % (city, api_key))
+        response = self.test_app.get(
+            '/%s/weatherdata?api_key=%s' % (self.city, self.api_key))
         weather_data = response.json
-        self.assertEqual(weather_data['name'], city)
+        self.assertEqual(weather_data['name'], self.city)
         self.assertEqual(weather_data['sys']['country'], 'HU')
         self.assertEqual(weather_data['timezone'], 7200)
         self.assertEqual(weather_data['id'], 3054643)
         self.assertEqual(weather_data['cod'], 200)
+
+    def test_query_city_data(self):
+        response = self.test_app.get(
+            '/%s?api_key=%s' % (self.city, self.api_key))
+        data = response.json
+        weather_data = data['weather_data']
+        self.assertEqual(weather_data['name'], self.city)
+        self.assertEqual(weather_data['sys']['country'], 'HU')
+        self.assertEqual(weather_data['timezone'], 7200)
+        self.assertEqual(weather_data['id'], 3054643)
+        self.assertEqual(weather_data['cod'], 200)
+
+    def test_city_temperature(self):
+        response = self.test_app.get(
+            '/%s/temperature?api_key=%s' % (self.city, self.api_key))
+        data = response.json
+        self.assertTrue(data.get("celsius") != None)
+        self.assertTrue(data.get("fahrenheit") != None)
+
+    def test_city_mood(self):
+        response = self.test_app.get(
+            '/%s/mood?api_key=%s' % (self.city, self.api_key))
+        data = response.json
+        self.assertTrue(data.get("mood") != None)
+
+    def test_time_until_sunrise_or_sunset(self):
+        weather_data = query_weather_data(self.city, self.api_key)
+        response = self.test_app.get(
+            '/%s/time_until_sunrise_or_sunset?api_key=%s' % (self.city, self.api_key))
+        data = response.json
+        if (datetime.datetime.now().timestamp() < weather_data['sys']['sunrise'] or
+                datetime.datetime.now().timestamp() > weather_data['sys']['sunset']):
+            self.assertTrue(data.get("until_sunrise") != None)
+        else:
+            self.assertTrue(data.get("until_sunset") != None)
+
 
 if __name__ == '__main__':
     unittest.main()
--- exercise_1_wmt_server.py
+++ exercise_1_wmt_server.py
@@ -1,10 +1,53 @@
-from bottle import route, run, Bottle, request, response
-from exercise_1_weathermonitoringtool import query_weather_data
+from bottle import Bottle, request
+from exercise_1_weathermonitoringtool import query_weather_data, temperature_to_celsius_and_fahrenheit
+from exercise_1_weathermonitoringtool import time_until_sunrise_or_sunset, predict_mood_from_weather
 
 wmt_server = Bottle()
-@wmt_server.route('/weatherdata')
-def weatherdata():
-    return query_weather_data(request.query.get("city"), request.query.get("api_key"))
+
+
+@wmt_server.route('/<city>')
+def cityfullinfo(city):
+    weather_data = query_weather_data(city, request.query.get("api_key"))
+    celsius, fahrenheit = temperature_to_celsius_and_fahrenheit(
+        weather_data['main']['temp'])
+    return {"name": city, "celsius": celsius, "fahrenheit": fahrenheit,
+            "mood": predict_mood_from_weather(weather_data), "weather_data": weather_data}
+
+
+@wmt_server.route('/<city>/weatherdata')
+def weatherdata(city):
+    return query_weather_data(city, request.query.get("api_key"))
+
+
+@wmt_server.route('/<city>/temperature')
+def citytemperature(city):
+    weather_data = query_weather_data(city, request.query.get("api_key"))
+    celsius, fahrenheit = temperature_to_celsius_and_fahrenheit(
+        weather_data['main']['temp'])
+    return {"celsius": celsius, "fahrenheit": fahrenheit}
+
+
+@wmt_server.route('/<city>/mood')
+def citymood(city):
+    weather_data = query_weather_data(city, request.query.get("api_key"))
+    return {"mood": predict_mood_from_weather(weather_data)}
+
+
+@wmt_server.route('/<city>/time_until_sunrise_or_sunset')
+def citytimeuntilsunriseorsunset(city):
+    weather_data = query_weather_data(city, request.query.get("api_key"))
+    untilwhat, time = time_until_sunrise_or_sunset(
+        weather_data['sys']['sunrise'], weather_data['sys']['sunset'])
+    return {untilwhat: time}
+
+
+"""@wmt_server.route('/comparecities')
+def comparecities():
+    weather_data1 = query_weather_data(request.query.get("city1"),request.query.get("api_key"))
+    weather_data2 = query_weather_data(request.query.get("city2"),request.query.get("api_key"))
+    weather_data3 = query_weather_data(request.query.get("city3"),request.query.get("api_key"))
+    weather_data4 = query_weather_data(request.query.get("city4"),request.query.get("api_key"))
+    """
 
 if __name__ == '__main__':
     wmt_server.run(host='localhost', port=8080)
