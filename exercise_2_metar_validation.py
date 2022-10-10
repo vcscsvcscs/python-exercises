d33670a072844f5379226c8ef69107890545f7c3
Vargha Csongor <Culisa1023@gmail.com>
2022-10-10 14:03:25 +0200

    Normalization of data, Pandas dataframe for better  proccesing

--- exercise_2_metar_validation.py
+++ exercise_2_metar_validation.py
@@ -1,3 +1,4 @@
+from dataclasses import field
 import requests
 import pandas as pd
 from exercise_1_weathermonitoringtool import query_weather_data, temperature_to_celsius_and_fahrenheit
@@ -9,27 +10,67 @@ def query_metar_data(lat,lon, api_key):
     response = requests.get(url, headers=headers)
     return response.json()
 
-def create_key_list(data):
-    data_keys = [] 
-    for field in data:
-        if(type(data[field]) is dict):
-            data_keys.append({field:create_key_list(data[field])})
-        else:
-            data_keys.append(field)
-    return data_keys
+def create_dataframe(datacity,datametar):
+    accuracy = {}
+    accuracy["header"] ="Data Accuracy in %"
+    for keyfield in datametar:
+        if keyfield in datacity and keyfield != "header":
+            accuracy[keyfield] = 100 - (abs(datametar[keyfield] - datacity[keyfield])/datametar[keyfield]*100)
+    print (accuracy)
+    dataframe = pd.DataFrame([accuracy,datametar,datacity]).dropna(axis=1)
+    return dataframe 
 
-def normalize_data(data):
-    df = pd.DataFrame(data)
-    df_list = list()
+def normalize_metardata(data):
+    data_normalized = {}
+    data = data["data"][0]
+    data_normalized["header"] = "METAR Data"
+    if(data.get("wind")is not None):
+        data_normalized["windspeed"] = data["wind"]["speed_mps"]
+        data_normalized["winddirection"] = data["wind"]["degrees"]
+    if(data.get("humidity")is not None):
+        data_normalized["humidity"] = data["humidity"]["percent"]
+    if(data.get("barometer")is not None):
+        data_normalized["pressure"] = data["barometer"]["mb"]
+    if(data.get("temperature")is not None):
+        data_normalized["temperatureincelsius"] = data["temperature"]["celsius"]
+        data_normalized["temperatureinfahrenheit"] = data["temperature"]["fahrenheit"]
+    if(data.get("visibility")is not None):
+        data_normalized["visibility"] = data["visibility"]["meters_float"]
+    if(data.get("rain") is not None):
+        data_normalized["rain"] = data["rain"]["inches"]*2.54
+    if(data.get("snow") is not None):
+        data_normalized["snow"] = data["snow"]["inches"]*2.54
+    if(data.get("sunrise") is not None):
+        data_normalized["sunrise"] = data["sunrise"]["unix"]
+    if(data.get("sunset") is not None):
+        data_normalized["sunset"] = data["sunset"]["unix"]
+    return data_normalized
 
-    for col in df.columns[3:]:
-        v = pd.json_normalize(df[col])
-        v.columns = [f'{col}_{c}' for c in v.columns]
-        df_list.append(v)
-
-    # combine into one dataframe
-    return pd.concat([df.iloc[:, :3]] + df_list, axis=2)
-
-
+def normalize_citydata(data):
+    data_normalized = {}    
+    data_normalized["header"] = "OpenWeather Data"
+    if(data['weather_data'].get("wind")is not None):
+        data_normalized["windspeed"] = data["weather_data"]["wind"]["speed"]
+        data_normalized["winddirection"] = data['weather_data']["wind"]["deg"]
+    if(data['weather_data']['main'].get("humidity")is not None):
+        data_normalized["humidity"] = data["weather_data"]["main"]["humidity"]
+    if(data['weather_data']['main'].get("pressure")is not None):
+        data_normalized["pressure"] = data["weather_data"]["main"]["pressure"]
+    data_normalized["temperatureincelsius"] = data["temperature"]['celsius']
+    data_normalized["temperatureinfahrenheit"] = data["temperature"]['fahrenheit']
+    if(data['weather_data'].get("visibility")is not None):
+        data_normalized["visibility"] = data["weather_data"]["visibility"]
+    if(data['weather_data'].get("rain")is not None):
+        data_normalized["rain"] = data["weather_data"]["rain"]["1h"]
+    if(data['weather_data'].get("snow")is not None):
+        data_normalized["snow"] = data["weather_data"]["snow"]["1h"]
+    if(data['weather_data']['sys'].get("sunrise")is not None):
+        data_normalized["sunrise"] = data["weather_data"]["sys"]["sunrise"]
+    if(data['weather_data']['sys'].get("sunset")is not None):
+        data_normalized["sunset"] = data["weather_data"]["sys"]["sunset"]
+    if(data['weather_data'].get("clouds")is not None):
+        data_normalized["clouds"] = data["weather_data"]["clouds"]["all"]
+    
+    return data_normalized
 #def find_common_fields(full_city_info, metar_data):
     
--- exercise_2_test_metar_validation.py
+++ exercise_2_test_metar_validation.py
@@ -1,5 +1,5 @@
 import unittest
-from exercise_2_metar_validation import query_metar_data, normalize_data,create_key_list
+from exercise_2_metar_validation import query_metar_data, create_dataframe, normalize_metardata, normalize_citydata
 from exercise_1_weathermonitoringtool import query_weather_data, temperature_to_celsius_and_fahrenheit
 from exercise_1_weathermonitoringtool import time_until_sunrise_or_sunset, predict_mood_from_weather,query_city_location
 
@@ -9,8 +9,8 @@ class TestMetarValidation(unittest.TestCase):
     Oapi_key = "76717af25a09a9c55ca00b44f771acc0"
     def test_query_metar_data(self):
         lat,lon = query_city_location(self.cities[1],self.Oapi_key)
-        data = query_metar_data(lat,lon, self.Mapi_key)
-        print(data["data"])
+        metardata = query_metar_data(lat,lon, self.Mapi_key)
+        print(metardata["data"][0],'\n')
         weather_data = query_weather_data(self.cities[1], self.Oapi_key)
         celsius, fahrenheit = temperature_to_celsius_and_fahrenheit(
             weather_data['main']['temp'])
@@ -18,8 +18,11 @@ class TestMetarValidation(unittest.TestCase):
             weather_data['sys']['sunrise'], weather_data['sys']['sunset'])
         data ={"name": self.cities[1], 'temperature':{"celsius": celsius, "fahrenheit": fahrenheit},
                 "mood": predict_mood_from_weather(weather_data), "weather_data": weather_data, untilsmth: time}
-        print(data)
-        print(create_key_list(data))
+        print(data,'\n')
+        print(normalize_citydata(data),'\n')
+        print(normalize_metardata(metardata),'\n')
+        print(create_dataframe(normalize_citydata(data),normalize_metardata(metardata)))
+        
         
 if __name__ == '__main__':
     unittest.main()
