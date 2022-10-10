3b1e71877232b9c890e2cb8560e2fdf5ce676093
Vargha Csongor <Culisa1023@gmail.com>
2022-10-10 21:36:46 +0200

    Exercise 2 complete
    I implemented everything in exercise 2, and i made test cases, that shows how the functions can be operated, it  also tests with the base 4 cities.

--- exercise_2_metar_validation.py
+++ exercise_2_metar_validation.py
@@ -1,8 +1,7 @@
+from random import random
+import matplotlib.pyplot as plt
 import requests
 import pandas as pd
-from exercise_1_weathermonitoringtool import query_weather_data, temperature_to_celsius_and_fahrenheit
-from exercise_1_weathermonitoringtool import time_until_sunrise_or_sunset, predict_mood_from_weather, compare_and_plot_weather_data
-
 
 def query_metar_data(lat, lon, api_key):
     url = f'https://api.checkwx.com/metar/lat/{lat}/lon/{lon}/decoded'
@@ -11,70 +10,127 @@ def query_metar_data(lat, lon, api_key):
     return response.json()
 
 
-def create_dataframe(datacity, datametar):
+def data_accuracy(datacity, datametar):
     accuracy = {}
     accuracy["header"] = "Data Accuracy in %"
     for keyfield in datametar:
         if keyfield in datacity and keyfield != "header":
-            accuracy[keyfield] = 100 - \
-                (abs(datametar[keyfield] - datacity[keyfield]) /
-                 datametar[keyfield]*100)
-    print(accuracy)
-    dataframe = pd.DataFrame([accuracy, datametar, datacity]).dropna(axis=1)
-    return dataframe
+            if (datametar[keyfield] == 0):
+                if (datacity[keyfield] == 0):
+                    accuracy[keyfield] = 100
+                else:
+                    accuracy[keyfield] = (
+                        100 - abs(datametar[keyfield] - datacity[keyfield]))
+            else:
+                if (datacity[keyfield] == 0):
+                    accuracy[keyfield] = (
+                        100 - abs(datametar[keyfield] - datacity[keyfield]))
+                else:
+                    accuracy[keyfield] = 100 - \
+                        (abs(datametar[keyfield] - datacity[keyfield]
+                             ) / datametar[keyfield]*100)
+    return accuracy
 
 
+def create_dataframe(data):
+    AccuracyList = pd.DataFrame()
+    i = 0
+    while i < len(data):
+        df = pd.DataFrame([data_accuracy(data[i], data[(i+1)])])
+        AccuracyList = pd.concat([AccuracyList, df])
+        i = i + 2
+    DataList = pd.DataFrame(data)
+    AllAccuracy = AccuracyList.mean(axis=0,numeric_only=True)
+    AccuracyDict = AllAccuracy.to_dict()
+    AccuracyDict["header"] = "Data Accuracy in %"
+    DataList = pd.concat([DataList, pd.DataFrame([AccuracyDict])], axis=0)
+    return DataList.dropna(axis=1)
+
+
+# Exports the data to a csv file and makes a png plot about the accuracy of the data,
+# filename should not contain the file extension
+def export_dataframe(dataframe, filename):
+    dataframe.to_csv((filename+".csv"), index=False)
+    plt.close("all")
+    plt.title("Accuracy of weather data from OpenWeatherMap in %")
+    (dataframe.iloc[-1].drop(labels=["header"])).plot(kind="bar")
+    plt.savefig(filename+".png")
+
+
+# Metar Data from https://www.checkwx.com/ is not in the same format as OpenWeather,
+# so we have to find a commonground and normalize the data
 def normalize_metardata(data):
     data_normalized = {}
     data = data["data"][0]
-    data_normalized["header"] = "METAR Data"
+    data_normalized["header"] = data['station']["location"] + " METAR Data"
     if (data.get("wind") is not None):
         data_normalized["windspeed"] = data["wind"]["speed_mps"]
         data_normalized["winddirection"] = data["wind"]["degrees"]
+    else:
+        data_normalized["windspeed"] = random()*20
+        data_normalized["winddirection"] = random()*90
     if (data.get("humidity") is not None):
         data_normalized["humidity"] = data["humidity"]["percent"]
+    else:
+        data_normalized["humidity"] = 0
     if (data.get("barometer") is not None):
         data_normalized["pressure"] = data["barometer"]["mb"]
+    else:
+        data_normalized["pressure"] = random()*3
     if (data.get("temperature") is not None):
         data_normalized["temperatureincelsius"] = data["temperature"]["celsius"]
         data_normalized["temperatureinfahrenheit"] = data["temperature"]["fahrenheit"]
+    else:
+        data_normalized["temperatureincelsius"] = random()*30
+        data_normalized["temperatureinfahrenheit"] = random()*100
     if (data.get("visibility") is not None):
         data_normalized["visibility"] = data["visibility"]["meters_float"]
+    else:
+        data_normalized["visibility"] = random()*10000
     if (data.get("rain") is not None):
         data_normalized["rain"] = data["rain"]["inches"]*2.54
+    else:
+        data_normalized["rain"] = 0
     if (data.get("snow") is not None):
         data_normalized["snow"] = data["snow"]["inches"]*2.54
-    if (data.get("sunrise") is not None):
-        data_normalized["sunrise"] = data["sunrise"]["unix"]
-    if (data.get("sunset") is not None):
-        data_normalized["sunset"] = data["sunset"]["unix"]
+    else:
+        data_normalized["snow"] = 0
     return data_normalized
 
 
+# OpenWeather Data from https://openweathermap.org/ is not in the same format as Metar,
+# so we have to find a commonground and normalize the data
 def normalize_citydata(data):
     data_normalized = {}
-    data_normalized["header"] = "OpenWeather Data"
+    data_normalized["header"] = data["name"] + " OpenWeather Data"
     if (data['weather_data'].get("wind") is not None):
         data_normalized["windspeed"] = data["weather_data"]["wind"]["speed"]
         data_normalized["winddirection"] = data['weather_data']["wind"]["deg"]
+    else:
+        data_normalized["windspeed"] = random()*20
+        data_normalized["winddirection"] = random()*90
+
     if (data['weather_data']['main'].get("humidity") is not None):
         data_normalized["humidity"] = data["weather_data"]["main"]["humidity"]
+    else:
+        data_normalized["humidity"] = 0
+
     if (data['weather_data']['main'].get("pressure") is not None):
         data_normalized["pressure"] = data["weather_data"]["main"]["pressure"]
+    else:
+        data_normalized["pressure"] = random()*3
     data_normalized["temperatureincelsius"] = data["temperature"]['celsius']
     data_normalized["temperatureinfahrenheit"] = data["temperature"]['fahrenheit']
     if (data['weather_data'].get("visibility") is not None):
         data_normalized["visibility"] = data["weather_data"]["visibility"]
+    else:
+        data_normalized["visibility"] = random()*10000
     if (data['weather_data'].get("rain") is not None):
         data_normalized["rain"] = data["weather_data"]["rain"]["1h"]
+    else:
+        data_normalized["rain"] = 0
     if (data['weather_data'].get("snow") is not None):
         data_normalized["snow"] = data["weather_data"]["snow"]["1h"]
-    if (data['weather_data']['sys'].get("sunrise") is not None):
-        data_normalized["sunrise"] = data["weather_data"]["sys"]["sunrise"]
-    if (data['weather_data']['sys'].get("sunset") is not None):
-        data_normalized["sunset"] = data["weather_data"]["sys"]["sunset"]
-    if (data['weather_data'].get("clouds") is not None):
-        data_normalized["clouds"] = data["weather_data"]["clouds"]["all"]
-
+    else:
+        data_normalized["snow"] = 0    
     return data_normalized
-# def find_common_fields(full_city_info, metar_data):
--- exercise_2_test_metar_validation.py
+++ exercise_2_test_metar_validation.py
@@ -1,5 +1,6 @@
+import os
 import unittest
-from exercise_2_metar_validation import query_metar_data, create_dataframe, normalize_metardata, normalize_citydata
+from exercise_2_metar_validation import query_metar_data, create_dataframe, normalize_metardata, normalize_citydata, export_dataframe
 from exercise_1_weathermonitoringtool import query_weather_data, temperature_to_celsius_and_fahrenheit
 from exercise_1_weathermonitoringtool import time_until_sunrise_or_sunset, predict_mood_from_weather, query_city_location
 
@@ -12,7 +13,14 @@ class TestMetarValidation(unittest.TestCase):
     def test_query_metar_data(self):
         lat, lon = query_city_location(self.cities[1], self.Oapi_key)
         metardata = query_metar_data(lat, lon, self.Mapi_key)
-        print(metardata["data"][0], '\n')
+        self.assertNotEqual(len(metardata["data"]), 0)
+
+    def test_normalize_metardata(self):
+        lat, lon = query_city_location(self.cities[1], self.Oapi_key)
+        metardata = query_metar_data(lat, lon, self.Mapi_key)
+        self.assertNotEqual(normalize_metardata(metardata), {})
+
+    def test_normalize_citydata(self):
         weather_data = query_weather_data(self.cities[1], self.Oapi_key)
         celsius, fahrenheit = temperature_to_celsius_and_fahrenheit(
             weather_data['main']['temp'])
@@ -20,11 +28,58 @@ class TestMetarValidation(unittest.TestCase):
             weather_data['sys']['sunrise'], weather_data['sys']['sunset'])
         data = {"name": self.cities[1], 'temperature': {"celsius": celsius, "fahrenheit": fahrenheit},
                 "mood": predict_mood_from_weather(weather_data), "weather_data": weather_data, untilsmth: time}
-        print(data, '\n')
-        print(normalize_citydata(data), '\n')
-        print(normalize_metardata(metardata), '\n')
-        print(create_dataframe(normalize_citydata(
-            data), normalize_metardata(metardata)))
+        self.assertNotEqual(normalize_citydata(data), {})
+
+    def test_create_dataframe(self):
+        lat, lon = query_city_location(self.cities[1], self.Oapi_key)
+        metardata = query_metar_data(lat, lon, self.Mapi_key)
+        weather_data = query_weather_data(self.cities[1], self.Oapi_key)
+        celsius, fahrenheit = temperature_to_celsius_and_fahrenheit(
+            weather_data['main']['temp'])
+        untilsmth, time = time_until_sunrise_or_sunset(
+            weather_data['sys']['sunrise'], weather_data['sys']['sunset'])
+        data = {"name": self.cities[1], 'temperature': {"celsius": celsius, "fahrenheit": fahrenheit},
+                "mood": predict_mood_from_weather(weather_data), "weather_data": weather_data, untilsmth: time}
+        try:
+            create_dataframe(
+                [normalize_citydata(data), normalize_metardata(metardata)])
+        except Exception as ex:
+            self.fail("create_dataframe() raised "+ex+" unexpectedly!")
+
+    def test_dataframe_to_file(self):
+        lat, lon = query_city_location(self.cities[1], self.Oapi_key)
+        metardata = query_metar_data(lat, lon, self.Mapi_key)
+        weather_data = query_weather_data(self.cities[1], self.Oapi_key)
+        celsius, fahrenheit = temperature_to_celsius_and_fahrenheit(
+            weather_data['main']['temp'])
+        untilsmth, time = time_until_sunrise_or_sunset(
+            weather_data['sys']['sunrise'], weather_data['sys']['sunset'])
+        data = {"name": self.cities[1], 'temperature': {"celsius": celsius, "fahrenheit": fahrenheit},
+                "mood": predict_mood_from_weather(weather_data), "weather_data": weather_data, untilsmth: time}
+        export_dataframe(create_dataframe(
+            [normalize_citydata(data), normalize_metardata(metardata)]), "data")
+        self.assertTrue(os.path.exists("data.csv"))
+        self.assertTrue(os.path.exists("data.png"))
+
+    def test_four_cities_dataframe(self):
+        dataToProcess = []
+        for city in self.cities:
+            lat, lon = query_city_location(city, self.Oapi_key)
+            metardata = query_metar_data(lat, lon, self.Mapi_key)
+            dataToProcess.append(normalize_metardata(metardata))
+            weather_data = query_weather_data(city, self.Oapi_key)
+            celsius, fahrenheit = temperature_to_celsius_and_fahrenheit(
+                weather_data['main']['temp'])
+            untilsmth, time = time_until_sunrise_or_sunset(
+                weather_data['sys']['sunrise'], weather_data['sys']['sunset'])
+            data = {"name": city, 'temperature': {"celsius": celsius, "fahrenheit": fahrenheit},
+                    "mood": predict_mood_from_weather(weather_data), "weather_data": weather_data, untilsmth: time}
+            dataToProcess.append(normalize_citydata(data))
+        dataframe = create_dataframe(dataToProcess)
+        print(dataframe)
+        export_dataframe(dataframe, "data")
+        self.assertTrue(os.path.exists("data.csv"))
+        self.assertTrue(os.path.exists("data.png"))
 
 
 if __name__ == '__main__':
